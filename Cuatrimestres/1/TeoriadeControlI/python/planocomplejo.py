db70bc99d31fffc24800ce3af5d23fd8c1fc0863
Roberto Cadena Vega <robblack00_7@hotmail.com>
2014-09-19 17:18:46 -0500

    Se termina codigo para grafica de plano complejo

--- Cuatrimestres/1/TeoriadeControlI/python/planocomplejo.py
+++ Cuatrimestres/1/TeoriadeControlI/python/planocomplejo.py
@@ -1,45 +1,75 @@
 #!/usr/bin/env python
 
 from pylab import *
-from matplotlib import style
+from numpy import exp, abs
 
-style.use("fivethirtyeight")
-
-# create some data to use for the plot
 dt = 0.001
-t = arange(0.0, 10.0, dt)
-r1 = exp(-t[:1000]/0.05)
-r2 = exp(t[:1000]/0.05)
+t = arange(0.0, 1.0, dt)
+r1 = exp(-t/0.05)
+r2 = exp(t/0.05)
+r3 = exp((-2+10j)*t/0.05).real
+r4 = exp((2+10j)*t/0.05).real
+r5 = exp((10j)*t/0.05).real
+r6 = [1 for i in t]
 
-
-# the main axes is subplot(111) by default
-plot(-5, 0, "x")
-plot([-2.5, -2.5], [2.5, -2.5], "x")
+# Grafica principal del plano complejo
+plot(-4, 0, "x")
+plot([-2, -2], [2, -2], "x")
 plot(0, 0, "x")
-plot([0, 0], [2.5, -2.5], "x")
-plot([2.5, 2.5], [2.5, -2.5], "x")
-plot(5, 0, "x")
-axis([-6, 6, -6, 6 ])
-title('Plano complejo')
-#grid(False)
+plot([0, 0], [2, -2], "x")
+plot([2, 2], [2, -2], "x")
+plot(4, 0, "x")
+
+axis([-5.5, 5.5, -4, 4])
+grid(True)
 xticks([0])
 yticks([0])
-#xticklabels([])
-#yticklabels([])
+a = gca()
+a.set_xticklabels([])
+a.set_yticklabels([])
 
-# this is another inset axes over the main axes
-a = axes([0.1, 0.5, 0.1, 0.1])
-plot(t[:len(r1)], r1,)
+# Grafica de comportamiento completamente amortiguado
+a = axes([0.18, 0.55, 0.1, 0.1])
+plot(t, r1)
 grid(True)
-setp(a, xlim=(-0.05, 0.2), ylim=(-0.05, 1),
+setp(a, xlim=(-0.02, 0.20), ylim=(-1.25, 1.25),
     xticks=[0], yticks=[0],
     xticklabels=[], yticklabels=[])
 
-# this is another inset axes over the main axes
-a = axes([0.8, 0.5, 0.1, 0.1])
-plot(t[:len(r2)], r2)
-setp(a, xlim=(-0.05, 0.2), ylim=(-0.05, 1),
+# Grafica de comportamiento exponencial
+a = axes([0.75, 0.55, 0.1, 0.1])
+plot(t, r2)
+setp(a, xlim=(-0.01, 0.10), ylim=(-5, 5),
     xticks=[0], yticks=[0],
     xticklabels=[], yticklabels=[])
 
-show()
+# Grafica de comportamiento sobreamortiguado
+a = axes([0.32, 0.15, 0.1, 0.1])
+plot(t, r3)
+setp(a, xlim=(-0.01, 0.1), ylim=(-1.25, 1.25),
+    xticks=[0], yticks=[0],
+    xticklabels=[], yticklabels=[])
+
+# Grafica de comportamiento subamortiguado
+a = axes([0.62, 0.15, 0.1, 0.1])
+plot(t, r4)
+setp(a, xlim=(-0.01, 0.1), ylim=(-15, 15),
+    xticks=[0], yticks=[0],
+    xticklabels=[], yticklabels=[])
+
+# Grafica de comportamiento subamortiguado
+a = axes([0.45, 0.75, 0.1, 0.1])
+plot(t, r5)
+setp(a, xlim=(-0.01, 0.1), ylim=(-1.25, 1.25),
+    xticks=[0], yticks=[0],
+    xticklabels=[], yticklabels=[])
+
+# Grafica de comportamiento subamortiguado
+a = axes([0.55, 0.45, 0.1, 0.1])
+plot(t, r6)
+setp(a, xlim=(-0.01, 0.1), ylim=(-1.25, 1.25),
+    xticks=[0], yticks=[0],
+    xticklabels=[], yticklabels=[])
+
+# Se guarda la figura en la misma carpeta
+savefig("planocomplejo.pdf")
