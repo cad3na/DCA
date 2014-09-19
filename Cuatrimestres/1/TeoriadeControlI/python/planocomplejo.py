1ef50719eec0e60944966f2b55f0d83a79f9cf8d
Roberto Cadena Vega <robblack00_7@hotmail.com>
2014-09-19 11:55:42 -0500

    Se avanza con la grafica del plano complejo

--- Cuatrimestres/1/TeoriadeControlI/python/planocomplejo.py
+++ Cuatrimestres/1/TeoriadeControlI/python/planocomplejo.py
@@ -8,27 +8,38 @@ style.use("fivethirtyeight")
 # create some data to use for the plot
 dt = 0.001
 t = arange(0.0, 10.0, dt)
-r = exp(-t[:1000]/0.05)               # impulse response
-x = randn(len(t))
-s = convolve(x,r)[:len(x)]*dt  # colored noise
+r1 = exp(-t[:1000]/0.05)
+r2 = exp(t[:1000]/0.05)
+
 
 # the main axes is subplot(111) by default
-plot(t, s)
-axis([-1, 1, 1.1*amin(s), 2*amax(s) ])
-xlabel('Reales')
-ylabel('Imaginarios')
+plot(-5, 0, "x")
+plot([-2.5, -2.5], [2.5, -2.5], "x")
+plot(0, 0, "x")
+plot([0, 0], [2.5, -2.5], "x")
+plot([2.5, 2.5], [2.5, -2.5], "x")
+plot(5, 0, "x")
+axis([-6, 6, -6, 6 ])
 title('Plano complejo')
-
-# this is an inset axes over the main axes
-a = axes([.65, .6, .2, .2], axisbg='y')
-n, bins, patches = hist(s, 400, normed=1)
-title('Probability')
-setp(a, xticks=[], yticks=[])
+#grid(False)
+xticks([0])
+yticks([0])
+#xticklabels([])
+#yticklabels([])
 
 # this is another inset axes over the main axes
-a = axes([0.2, 0.6, .2, .2], axisbg='y')
-plot(t[:len(r)], r)
-title('Impulse response')
-setp(a, xlim=(0,.2), xticks=[], yticks=[])
+a = axes([0.1, 0.5, 0.1, 0.1])
+plot(t[:len(r1)], r1,)
+grid(True)
+setp(a, xlim=(-0.05, 0.2), ylim=(-0.05, 1),
+    xticks=[0], yticks=[0],
+    xticklabels=[], yticklabels=[])
+
+# this is another inset axes over the main axes
+a = axes([0.8, 0.5, 0.1, 0.1])
+plot(t[:len(r2)], r2)
+setp(a, xlim=(-0.05, 0.2), ylim=(-0.05, 1),
+    xticks=[0], yticks=[0],
+    xticklabels=[], yticklabels=[])
 
 show()
