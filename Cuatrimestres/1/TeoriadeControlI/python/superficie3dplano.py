0402d39b5ae9facaa7aa8cc78263aaf42ab00ab2
Roberto Cadena Vega <robblack00_7@hotmail.com>
2014-11-23 19:53:30 -0600

    Se agrega grafica de plano tangente a superficie de nivel

--- Cuatrimestres/1/TeoriadeControlI/python/superficie3dplano.py
+++ Cuatrimestres/1/TeoriadeControlI/python/superficie3dplano.py
@@ -17,30 +17,27 @@ h = lambda r: (r**2 - 1)**2
 
 X, Y, Z = f(R, P), g(R, P), h(R)
 
-# point0 = array([2, 2, h(sqrt(eta[0]**2 + eta[1]**2 + eta[2]**2))])
-point0 = array([0, 0, 0])
+rho = sqrt(eta[0]**2 + eta[1]**2 + eta[2]**2)
+
+point0 = array([1 + rho, 1 - rho, h(rho)])
 normal = array([1, -1, 1])
 
-d0 = -point0.dot(normal)
+d = -point0.dot(normal)
 
-y = linspace(-0.5, 0.5, 100)
-z = linspace(-0.5, 0.5, 100)
-yy, zz = meshgrid(y, z)
+x = linspace(1.125, 1.375, 100)
+y = linspace(0.625, 0.875, 100)
 
-x0 = (-normal[1] * yy - normal[2] * zz - d0) * 1. /normal[0]
+xx, yy = np.meshgrid(x, y)
+z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
 
 a3d = gca(projection='3d')
-a3d.plot_surface(x0, yy, zz, alpha=1, color="r")
-a3d.plot_surface(X, Y, Z, alpha=0.15)
-
+a3d.plot_surface(xx, yy, z, alpha=0.25, color="r")
+a3d.plot_surface(X, Y, Z, alpha=0.35)
+#
 o = array([0, 0, 0])
 ex = array([1.75, 0, 0]) + o
 ey = array([0, 1.75, 0]) + o
 ez = array([0, 0, 1.0]) + o
-
-# a3d.text(ex[0] + 0.1, ex[1], ex[2], r'$v_1$', fontsize=20)
-# a3d.text(ey[0] + 0.05, ey[1], ey[2], r'$v_2$', fontsize=20)
-# a3d.text(ez[0], ez[1] - 0.05, ez[2] + 0.1, r'$f_1(v)$', fontsize=20)
 
 xs, ys, zs = zip(o, ex)
 plot(xs, ys, zs)
@@ -49,34 +46,28 @@ plot(xs, ys, zs)
 xs, ys, zs = zip(o, ez)
 plot(xs, ys, zs)
 
-xs, ys, zs = [v0[0], v0[0] + eta[0]], [v0[1], v0[1] + eta[1]], [v0[2] + 1, h(sqrt(eta[0]**2 + eta[1]**2+ eta[2]**2))]
+xs, ys, zs = [v0[0] + eta[0]], [v0[1] + eta[1]], [h(sqrt(eta[0]**2 + eta[1]**2+ eta[2]**2))]
 a3d.scatter(xs, ys, zs)
-
-xs1, ys1, zs1 = zip([0, v0[1], 0], v0, [v0[0], 0, 0])
-plot(xs1, ys1, zs1, "--")
-plot([xs[0], xs1[1]], [ys[0], ys1[1]], [zs[0], zs1[1]], "--")
 
 xs1, ys1, zs1 = zip([0, v0[1] + eta[1], 0], v0 + eta, [v0[0] + eta[0], 0, 0])
 plot(xs1, ys1, zs1, "--")
-plot([xs[1], xs1[1]], [ys[1], ys1[1]], [zs[1], zs1[1]], "--")
+plot([xs[0], xs1[1]], [ys[0], ys1[1]], [zs[0], zs1[1]], "--")
 
-# a3d.quiver( o[0] + eta[0],  o[0] + eta[1],  o[0] + eta[2], 1, -1, 0, linewidth=2, alpha=0.3, length=0.3)
-# a3d.quiver(v0[0] + eta[0], v0[1] + eta[1], v0[2] + eta[2], 1, -1, 0, linewidth=2, alpha=0.3, length=0.3)
+a3d.quiver(xs[0] + 0.2, ys[0] - 0.2, zs[0] + 0.2, 1, -1, 1, linewidth=2, alpha=1, length=0.35)
+a3d.quiver(xs[0] + 0.14, ys[0] - 0.14, zs[0] - 0.14, 1, -1, -1, linewidth=2, alpha=1, length=0.25)
 
-# a3d.text( o[0] + eta[0] + 0.05,  o[0] + eta[1],         o[0] + eta[2], r'$\eta$', fontsize=20)
-# a3d.text(v0[0] + eta[0] + 0.1,  v0[1] + eta[1] - 0.05, v0[2] + eta[2], r'$v_0 + \alpha \eta$', fontsize=20)
-# a3d.text(v0[0] - 0.05,          v0[1] + 0.1,           v0[2] , r'$v_0$', fontsize=20)
-a3d.text(v0[0],                 v0[1] + 0.05,          v0[2] + 1, r'$f_1(v_0)$', fontsize=20)
-a3d.text(v0[0] + eta[0],        v0[1] + eta[1] + 0.05, zs[1], r'$f_1(v_0 + \alpha \eta)$', fontsize=20)
+a3d.text(v0[0] + eta[0] - 0.025, v0[1] + eta[1] - 0.1, zs[0], r'$\mathcal{G}(v_0)$', fontsize=20)
+a3d.text(v0[0] + eta[0] + 0.05, v0[1] + eta[1] + 0.1, zs[0], r'$\nabla_v \mathcal{G}(v_0)$', fontsize=20)
+a3d.text(v0[0] + eta[0] + 0.175, v0[1] + eta[1] - 0.2, zs[0], r'$\frac{d R(t)}{dt}$', fontsize=20)
 
 grid(False)
 a = gca()
 xticks([])
 yticks([])
 a.set_zticks([])
-a.set_zlim3d([0, 1])
-a.set_ylim3d([0, 1.5])
-a.set_xlim3d([0, 1.5])
+a.set_zlim3d([0.5, 1])
+a.set_ylim3d([0.75, 1.25])
+a.set_xlim3d([0.75, 1.25])
 a.set_xticklabels([])
 a.set_yticklabels([])
 a.set_zticklabels([])
